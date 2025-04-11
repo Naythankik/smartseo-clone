8a25cae36a166d73d2ff54845d718a8658a5ae09
Naythankik <dolapokiki3ird@gmail.com>
2025-04-11 15:14:15 +0100

    - wip

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -4,20 +4,19 @@ import { FaCaretLeft, FaCaretRight } from "react-icons/fa";
 const TestimonialArticles = ({ id, name, description, level, image }) => {
     return (
         <article id={id}
-                 className="min-w-full transition-shadow duration-300 flex gap-5 items-center flex-col text-center">
+                 className="min-w-full transition-shadow duration-300 flex gap-3 md:gap-5 items-center flex-col text-center">
             {image && (
-                <img src={image} alt={name} className="rounded-full w-[100px] h-[100px]" />
+                <img src={image} alt={name} className="rounded-full w-[50px] md:w-[100px] h-[50px] md:h-[100px]" />
             )}
-            <p className="capitalize text-blue-950 text-xl font-semibold w-3/5">{description}</p>
-            <span className="text-cyan-400 text-8xl font-extrabold h-12">''</span>
+            <p className="capitalize text-blue-950 text-base md:text-xl font-semibold w-full md:w-3/5">{description}</p>
+            <span className="text-cyan-400 text-3xl md:text-6xl lg:text-8xl font-extrabold h-6 md:h-12">''</span>
             <div className="flex flex-col gap-2">
-                <span className="capitalize text-blue-950 text-2xl font-semibold">{name}</span>
-                <span className="capitalize text-gray-300 text-xl font-medium">{level}</span>
+                <span className="capitalize text-blue-950 text-base md:text-lg lg:text-2xl font-semibold">{name}</span>
+                <span className="capitalize text-gray-500 text-base md:text-xl font-medium">{level}</span>
             </div>
         </article>
     );
 };
-
 
 const TestimonialSlider = ({ testimonials }) => {
     const scrollRef = useRef();
@@ -38,7 +37,7 @@ const TestimonialSlider = ({ testimonials }) => {
             className="relative flex flex-col h-auto px-0 my-16 md:my-24 w-[90%] md:w-[85%] mx-auto gap-1 md:gap-8 lg:gap-12 justify-center items-center">
             <button
                 onClick={() => scroll("left")}
-                className="absolute top-1/2 -translate-y-1/2 left-4 bg-transparent border-2 border-gray-300 text-gray-600 w-12 h-12 rounded-full flex justify-center items-center text-2xl z-10"
+                className="absolute top-1/2 -translate-y-1/2 left-4 bg-transparent border-2 border-gray-300 text-gray-600 w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center text-lg md:text-2xl z-10"
             >
                 <FaCaretLeft/>
             </button>
@@ -55,7 +54,7 @@ const TestimonialSlider = ({ testimonials }) => {
 
             <button
                 onClick={() => scroll("right")}
-                className="absolute top-1/2 -translate-y-1/2 right-4 bg-transparent border-2 border-gray-300 text-gray-600 w-12 h-12 rounded-full flex justify-center items-center text-2xl z-10"
+                className="absolute top-1/2 -translate-y-1/2 right-4 bg-transparent border-2 border-gray-300 text-gray-600 w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center text-lg md:text-2xl z-10"
             >
                 <FaCaretRight/>
             </button>
